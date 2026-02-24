c3ac4a6b72cdd3f48de71620ac1f68ff954b3a25
Mochammad Jihan Isfalana <wallbreker22@gmail.com>
2026-02-24 16:03:01 +0700

    Updated Main.py part 1.1

--- backend_core/app/main.py
+++ backend_core/app/main.py
@@ -1,33 +1,56 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import sys
 import os
 
-# Trik biar Python bisa nemu file .so hasil compile C++ di folder root
+# Setup Path biar C++ kebaca
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# Import Modul C++ (Nama harus sama kayak di PYBIND11_MODULE)
 try:
     import survival_lib
 except ImportError:
-    survival_lib = None # Fallback kalau belum dicompile
+    survival_lib = None 
 
 app = FastAPI(title="College Budgeting API")
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
 def read_root():
-    return {"message": "Server Backend Aktif!", "cpp_status": "Ready" if survival_lib else "Not Compiled"}
+    return {"status": "Active", "engine": "C++ Ready" if survival_lib else "Missing"}
 
 @app.get("/cek-survival")
 def cek_survival(saldo: float, pengeluaran: float):
     if not survival_lib:
         return {"error": "Modul C++ belum di-compile!"}
     
-    # PANGGIL FUNGSI C++ DISINI
+    # 1. Panggil Otak C++
     sisa_hari = survival_lib.prediksi_bangkrut(saldo, pengeluaran)
     
+    # 2. Logic "Black Zone"
+    if sisa_hari <= 0:
+        status_hari = "Tidak Ada Sisa "
+        pesan = "💀 BLACK ZONE!!! HATI-HATI SEGERA KELUAR DARI ZONA INI!!"
+        zona = "black"
+    elif sisa_hari < 20:
+        status_hari = sisa_hari
+        pesan = "⚠️ RED ZONE!! Hemat lagi ya, jangan boros-boros!"
+        zona = "red"
+    else:
+        status_hari = sisa_hari
+        pesan = "✅ Green Zone. Aman."
+        zona = "green"
+
     return {
         "saldo": saldo,
         "pengeluaran_harian": pengeluaran,
-        "sisa_hari_bertahan": sisa_hari,
-        "pesan": "Hemat bang!" if sisa_hari < 30 else "Aman sentosa."
+        "sisa_hari_bertahan": status_hari,
+        "pesan": pesan,
+        "zona": zona 
     }
